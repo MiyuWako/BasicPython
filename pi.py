c91e40d8637c4cbc2d2cdb38a14e3eb3c0043daf
MiyuWako <wakomiyu111@gmail.com>
2023-09-24 22:00:14 +0900

    python_intro

--- pi.py
+++ pi.py
@@ -1,3 +1,5 @@
+import os
+
 text = """
     How I want a drink, alcoholic of course, after the heavy chapters involving
     quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.
@@ -5,3 +7,10 @@ text = """
 
 # TODO
 
+num_char=list(map(len,["How", "I", "want", "a", "drink","alcoholic","of",
+"course","after","the", "heavy","chapters","involving",
+"quantum","mechanics","All","of","thy","geometry","Herr","Planck","is",
+"fairly","hard"]))
+
+n=int("".join(map(str,num_char)))
+print(n)
