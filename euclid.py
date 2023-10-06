d01239200684208a64ccc585cec3c380114b0380
MiyuWako <wakomiyu111@gmail.com>
2023-10-06 17:20:52 +0900

    submit_1

--- euclid.py
+++ euclid.py
@@ -10,6 +10,13 @@ def gcd(a, b):
         return a
     else:
         return gcd(b, a%b) #再帰定義
+
+def gcd2(a, b): #a,bが互いに素であるかを判定する関数
+    if gcd(a,b)==1:
+        return True 
+    else:
+        return False
 print(gcd(a, b))
+print(gcd2(a, b))
 
 #bが0になるまで再帰的に自分自身を呼び出し、bが0になればaが最大公約数である   
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,17 +1,31 @@
-from math import sin,pi
+from math import exp, sin,pi
 # --example--
 # print(sin(0))
 # >>> 0
 # -----------
 
-a = 0
-b = pi / 2
-n = 100
-h = (b - a) /n
-s = 0
+#a = 0
+#b = pi / 2
+#n = 100
 
-for k in range(n):
-    s +=  (h / 2)*(sin(a + k*h) + sin(a + (k+1)*h))
-print(s)
+def integral(f, a=0, b=1, n=100):
+    s = 0
+    h = (b - a) /n
+    for k in range(n):
+        s +=  (h / 2)*(f(a + k*h) + f(a + (k+1)*h))
+    return s
 
+def f1(x):
+    return sin(x)
+def f2(x):
+    return 4/(1+x**2)
+def f3(x):
+    return (pi**(1/2))*exp(-x*x)
+print("(1)の積分値は")
+print(integral(f1, 0, pi/2, 50))
 
+print("(2)の積分値は")
+print(integral(f2, 0, 1, 100))
+
+print("(3)の積分値は")
+print(integral(f3, -100, 100, 1000))
