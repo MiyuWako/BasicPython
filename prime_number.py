be296c72727971ec707a8a2866d7d7cde7ab08a2
MiyuWako <wakomiyu111@gmail.com>
2023-10-06 13:15:45 +0900

    1 to i changed

--- prime_number.py
+++ prime_number.py
@@ -14,7 +14,7 @@ def isprime(n):
     if n % 2 ==0:
         return False
     for i in range(3, int(n**0.5)+1, 2):
-        if n % 1 == 0:
+        if n % i == 0:
             return False
     return True
 
